118912b11693bfa6b1a5ade974adc5e7efe75d23
daji daji <dora_tan_sie_min@temasekjc.moe.edu.sg>
2024-09-21 19:17:36 +0800

    trying to commit

--- mp_sort/app/static/library.py
+++ mp_sort/app/static/library.py
@@ -17,42 +17,60 @@ def generate():
 	array = gen_random_int(number, seed)
  
 	# convert the items into one single string
-	array_str = ''
- 
 	# the number should be separated by a comma
+	# and a full stop should end the string.
+	array_str = str(array)
+	array_str.remove('[', ']')
+	array_str.append('.')
+ 
+	# another method
+	array_str = ''
 	for item in array[:-1]:
 		array_str.append(str(item), ', ')
-  
-	# and a full stop should end the string.
-	array_str.append('.')
+	array_str.append(str(array[-1]), '.')
 
 	# This line is to placed the string into the HTML
 	# under div section with the id called "generate"	
 	document.getElementById("generate").innerHTML = array_str
+	# document.getElementbyId("generate").innerHTML
 	return array_str
 
 
 def sortnumber1():
 	'''	This function is used in Exercise 1.
 		The function is called when the sort button is clicked.
-
-		You need to do the following:
-		- get the list of numbers from the "generate" HTML id, use document.getElementById(id).innerHTML
-		- create a list of integers from the string of numbers
-		- call your sort function, either bubble sort or insertion sort
-		- create a string of the sorted numbers and store it in array_str
 	'''
 	# get the list of numbers from the "generate" HTML id, use document.getElementById(id).innerHTML
-	document.getElementById("generate").innerHTML = array_str
+	document.getElementById("generate").innerHTML = array
  
 	# create a list of integers from the string of numbers
-	numstring = gen_random_int
+	numslist = gen_random_int()
  
 	# call your sort function, either bubble sort or insertion sort
-		
+	end = len(numstring)
+	is_sorted = False # check if the string is sorted
+ 
+	while not is_sorted:        
+		for ptr in range(1, end):
+			rightptr = ptr
+			leftptr = rightptr - 1 # left side of the item of interest will be sorted
+			
+			while leftptr >= 0 and numslist[rightptr] < numslist[leftptr]:
+				numslist[rightptr], numslist[leftptr] = numslist[leftptr], numslist[rightptr]
+				leftptr -= 1
+				rightptr -= 1
+				# by the time the pointer reaches the end, all elements should have been sorted
+				is_sorted = True
+			
+ 
 	# create a string of the sorted numbers and store it in array_str
-	
-	array_str = None
+ 	# array_str = str(elements for elements in array) # idk if it works liddis
+	# array_str = ''
+	# for elements in array:
+	# 	array_str.append(str(elements))
+	array_str = str(numslist)
+	array_str.remove('[', ']')
+	array_str.append('.')
 	
 	document.getElementById("sorted").innerHTML = array_str
 
